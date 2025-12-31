04c7c505a421fd0a9083d572b2a7214ae0f1d956
FacuNote <facujanssen@hotmail.com>
2025-12-31 17:46:25 -0300

    bg content

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import "./App.css";
-
 import NavBar from "./components/NavBar/NavBar";
 import Content from "./components/Content/Content";
-
 
 function App() {
     return (
         <>
             <NavBar />
-            <Content />
+            <Content className="content" />
         </>
     );
 }
