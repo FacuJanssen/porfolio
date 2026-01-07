fbec8d2dc21f5b48dccea0568a38a46c33079811
Facu Janssen <facujanssen@hotmail.com>
2026-01-07 17:49:48 -0300

    flexbox

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,6 +3,7 @@ import "./Content.css";
 import Hero from "../Hero/Hero";
 import Technologies from "../Technologies/Technologies";
 import AboutMe from "../AboutMe/AboutMe";
+import "./Content.css";
 
 const Content = () => {
     return (
